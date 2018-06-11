9bf3a26e5b37e03f11d49f63ccee8193313a41ed
Felipe Venceslau <31327811+venceslaufelipe@users.noreply.github.com>
2018-06-11 19:04:10 -0300

    Update serie.ts

--- Spotflix/video/serie.ts
+++ Spotflix/video/serie.ts
@@ -1,7 +1,29 @@
-import {Produto} from '../produto'
-import {Video} from './video'
+import {Video} from './filme.ts';
 
-class Serie extends Video {
-    numeroDeEpisodios : number;
-    duracaoMediaDeEpisodio : number;
+class Serie extends Video{
+	private numeroDeEpisodios : number;
+	private duracaoMediaDeEpisodio : number;
+
+	public constructor(numeroDeEpisodios : number, duracaoMediaDeEpisodio : number){
+		super(anoLancamento, faixaEtaria);
+		this.numeroDeEpisodios = numeroDeEpisodios;
+		this.duracaoMediaDeEpisodio = duracaoMediaDeEpisodio;
+	}
+
+	public getNumeroDeEpisodios() : number{
+		return this.numeroDeEpisodios;
+	}
+
+	public setNumeroDeEpisodios(numeroDeEpisodios : number) : void{
+		this.numeroDeEpisodios = numeroDeEpisodios;
+	}
+
+	public getDuracaoMediaDeEpisodio() : number{
+		return this.duracaoMediaDeEpisodio;
+	}
+
+	public setDuracaoMediaDeEpisodio(duracaoMediaDeEpisodio : number) : void{
+		this.duracaoMediaDeEpisodio = duracaoMediaDeEpisodio;
+	}
+	
 }
