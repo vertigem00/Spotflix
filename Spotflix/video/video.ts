ed0f3aee1fabc633d776d5cb336f6632eea77438
Felipe Venceslau <31327811+venceslaufelipe@users.noreply.github.com>
2018-06-11 19:03:34 -0300

    Update video.ts

--- Spotflix/video/video.ts
+++ Spotflix/video/video.ts
@@ -1,6 +1,27 @@
-import {Produto} from '../produto'
+import {Produto} from './produto.ts'
 
-export abstract class Video extends Produto {
-    anoLancamento : number;
-    faixaEtaria : number;
+export abstract class Video extends Produto{
+	private anoLancamento : number;
+	private faixaEtaria : number;
+
+	public constructor(anoLancamento : number, faixaEtaria : number){
+		this.anoLancamento = anoLancamento;
+		this.faixaEtaria = faixaEtaria;
+	}
+
+	public getAnoLancamento() : number{
+		return this.anoLancamento;
+	}
+
+	public setAnoLancamento(anoLancamento : number) : void{
+		this.anoLancamento = anoLancamento;
+	}
+
+	public getFaixaEtaria() : number{
+		return this.faixaEtaria;
+	}
+
+	public setFaixaEtaria(faixaEtaria : number) : void{
+		this.faixaEtaria = faixaEtaria;
+	}
 }
