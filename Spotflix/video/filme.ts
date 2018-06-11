988c140818f2c0639cc6bd04a690b258c362292f
Felipe Venceslau <31327811+venceslaufelipe@users.noreply.github.com>
2018-06-11 19:05:01 -0300

    Update filme.ts

--- Spotflix/video/filme.ts
+++ Spotflix/video/filme.ts
@@ -1,6 +1,18 @@
-import {Produto} from '../produto'
-import {Video} from './video'
+import {Video} from './filme.ts';
 
-class Filme extends Video {
-    duracao : number;
+class Filme extends Video{
+	private duracao : number;
+
+	public constructor(duracao : number){
+		super(anoLancamento, faixaEtaria);
+		this.duracao = duracao;
+	}
+
+	public getDuracao() : number{
+		return this.duracao;
+	}
+
+	public setDuracao(duracao : number) : void{
+		this.duracao = duracao;
+	}
 }
