f36d57e9a6a579bcb1539522d5c706b6fd5a7f10
Felipe Venceslau <31327811+venceslaufelipe@users.noreply.github.com>
2018-06-11 19:05:31 -0300

    Update produto.ts

--- Spotflix/produto.ts
+++ Spotflix/produto.ts
@@ -1,12 +1,47 @@
+export abstract class Produto{
+	private codigo : string;
+	private nome : string;
+	private genero : string;
+	private preco : number;
 
+	public constructor(codigo : string, nome : string, genero : string, preco : number){
+		this.codigo = codigo;
+		this.nome = nome;
+		this.genero = genero;
+		this.preco = preco;
+	}
 
-export abstract class Produto {
-    codigo : string;
-    nome : string;
-    genero : string;
-    preco : number;
+	public getCogido() : string{
+		return this.codigo;
+	}
 
+	public setCodigo(codigo : string) : void{
+		this.codigo = codigo;
+	}
 
+	public getNome() : string{
+		return this.nome;
+	}
 
-    abstract executar() : void
+	public setNome(nome : string) : void{
+		this.nome = nome;
+	}
+
+	public getGenero() : string{
+		return this.genero;
+	}
+
+	public setGenero(genero : string) : void{
+		this.genero = genero;
+	}
+
+	public getPreco() : number{
+		return this.preco;
+	}
+
+	public setPreco(preco : number) : void{
+		this.preco = preco;
+	}
+
+	public abstract executar() : void;
 }
