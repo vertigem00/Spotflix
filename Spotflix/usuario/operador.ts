513e1243beb6ca253f5349f186cbb45dc254c8db
Felipe Venceslau <31327811+venceslaufelipe@users.noreply.github.com>
2018-06-12 18:31:18 -0300

    Update operador.ts

--- Spotflix/usuario/operador.ts
+++ Spotflix/usuario/operador.ts
@@ -2,7 +2,7 @@ import {Funcionario} from './funcionario';
 import {Cliente} from './cliente';
 import {Produto} from '../produto';
 
-class OperadorDeSistema extends Funcionario{
+export class OperadorDeSistema extends Funcionario{
 
 	public constructor(matricula : string, salario : number, nome : string, email : string, senha : string){
 		super(matricula, salario, nome, email, senha);
