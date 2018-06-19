f1d9b8466d71f9651c5afb7cd9479dae78bc52fc
Felipe Venceslau <31327811+venceslaufelipe@users.noreply.github.com>
2018-06-18 21:14:12 -0300

    Add files via upload

--- Spotflix/usuario/RepositorioDeProduto.ts
+++ Spotflix/usuario/RepositorioDeProduto.ts
@@ -1,22 +1,89 @@
 import {Produto} from '../produto';
+import {Filme} from '../video/filme';
+import {Serie} from '../video/Serie'
+import {Musica} from '../audio/musica';
+import {Podcast} from '../audio/podcast';
+import {Pacote} from '../Pacote';
+import {Data} from '../Data';
 
 export class RepositorioDeProduto{
-	private repositorio : Map<string, Produto>;
+	mapaProduto : Map<string, Produto>;
+    mapaFilme: Map<string, Filme>;
+    mapaSerie: Map<string, Serie>;
+    mapaMusica: Map<string, Musica>;
+    mapaPodcast: Map<string, Podcast>;
+    mapaPacote: Map<string, Pacote>;
 
 	public constructor(){
-		this.repositorio = new Map<string, Produto>()
+    	this.mapaProduto = new Map<string, Produto>();
+    	this.mapaFilme = new Map<string, Filme>();
+    	this.mapaSerie = new Map<string, Serie>();
+    	this.mapaMusica = new Map<string, Musica>();
+    	this.mapaPodcast = new Map<string, Podcast>();
+    	this.mapaPacote = new Map<string, Pacote>();
+
 	}
 
-	public adicionarUsuario(produto : Produto) : boolean{
-		this.repositorio.set(produto.getCogido(), produto);
-		return true;
+	public adicionarProduto ( codigo:string, Produto: Produto) : void { 
+    	this.mapaProduto.set(codigo, Produto);
 	}
 
-	public removerUsuario(codigo : string) : boolean{
-		return this.repositorio.delete(codigo);
+	public adicionarFilme ( codigo:string, nome:string, genero:string, preco:number, 
+    	anoLocamento:number, faixaEtaria:number, duracao:number) : void {
+
+    	let filme = new Filme (codigo, nome, genero, preco, anoLocamento, faixaEtaria, duracao);
+    	this.mapaFilme.set(codigo, filme);
+    	this.adicionarProduto(codigo, filme);
 	}
 
-	public retornarUsuario(codigo : string) : Produto|undefined{
-		return this.repositorio.get(codigo);
+	public adicionarSerie ( codigo:string, nome:string, genero:string, preco:number, 
+    	anoLocamento:number, faixaEtaria:number, numeroDeEpisodios : number, duracaoMediaDeEpisodio : number ) : void {
+
+    	let serie = new Serie (codigo, nome, genero, preco, anoLocamento, faixaEtaria, numeroDeEpisodios, duracaoMediaDeEpisodio);
+    	this.mapaSerie.set(codigo, serie);
+    	this.adicionarProduto(codigo, serie);
+	}
+
+	public adicionarMusica ( codigo:string, nome:string, genero:string, preco:number, autor:string, duracao:number, album:string) : void {
+    	let musica = new Musica ( codigo, nome, genero, preco, autor, duracao, album);
+    	this.mapaMusica.set(codigo, musica);
+    	this.adicionarProduto(codigo, musica);
+	}
+
+	public adicionarPodcast ( codigo:string, nome:string, genero:string, preco:number, autor:string, duracao:number, tematica:string) : void {
+    	let podcast = new Podcast ( codigo, nome, genero, preco, autor, duracao, tematica);
+    	this.mapaPodcast.set(codigo, podcast);
+    	this.adicionarProduto(codigo, podcast);
+	}
+
+	public adicionarPacote ( codigo:string, nome:string, genero:string, preco:number, 
+    	produtos:Array<Produto>, validade: Data, precoDeProtudos:number) : void {
+
+    	let pacote = new Pacote (codigo, nome, genero, preco, produtos, validade, precoDeProtudos);
+    	this.mapaPacote.set(codigo, pacote);
+    	this.adicionarProduto(codigo, pacote);
+	}
+
+	public listarProdutos (){
+    	this.mapaProduto.forEach( (valor, chave) => {
+        	console.log(valor);
+    	});
+	}
+
+	public procurarProduto ( codigo:string ){
+    	if(this.mapaProduto.has(codigo)){
+        	console.log(this.mapaProduto.get(codigo));
+    	}else{
+    		console.log("Produto não encontrado");
+    	}
+	}
+
+	public removerProduto (codigo: string) : boolean{
+    	if (!this.mapaProduto.has(codigo)){
+        	return false;
+    	}
+
+    	this.mapaProduto.delete(codigo);
+    	return true;
 	}
 }
--- Spotflix/usuario/RepositorioDeUsuario.ts
+++ Spotflix/usuario/RepositorioDeUsuario.ts
@@ -1,22 +1,84 @@
-import {Usuario} from './usuario';
+import {Usuario} from "./Usuario";
+import {Cliente} from './Cliente';
+import {OperadorDeSistema} from './operador';
+import {Funcionario} from './Funcionario';
+import {Gerente} from "./Gerente";
 
 export class RepositorioDeUsuario{
-	private repositorio : Map<string, Usuario>;
+    public operadores : Array<OperadorDeSistema>;
+    private mapadeusuario : Map<string,Usuario>;
+    public clientes  : Array<Cliente>;
+    public  usuarios : Array<Usuario>;
 
-	public constructor(){
-		this.repositorio = new Map<string, Usuario>()
-	}
+    public constructor(){
+    	this.clientes = new Array<Cliente>();
+    	this.mapadeusuario  = new Map<string, Usuario>();
+    	this.operadores = new Array<OperadorDeSistema>();
+    	this.usuarios = new Array<Usuario>();
 
-	public adicionarUsuario(usuario : Usuario) : boolean{
-		this.repositorio.set(usuario.getEmail(), usuario);
-		return true;
-	}
+    }
 
-	public removerUsuario(email : string) : boolean{
-		return this.repositorio.delete(email);
-	}
+    public procurarUsuarioLogado(email: string) : Usuario | null {
+        for(let usuario of this.usuarios){
+            if(usuario.getEmail() == email){
+                if(usuario instanceof Gerente || usuario instanceof Cliente || usuario instanceof OperadorDeSistema){
+                    return usuario;
+                }
+            }
+        }
 
-	public retornarUsuario(email : string) : Usuario|undefined{
-		return this.repositorio.get(email);
-	}
+        return null;
+    }
+
+    public removerOperador(email: string):boolean{
+        for(let i = 0; i < this.usuarios.length; i++){
+            if(this.usuarios[i].getEmail() == email){
+                this.usuarios.splice(i, 1);
+                for(let j = 0; j < this.operadores.length; j++){
+                    if(this.operadores[j].getEmail() == email){
+                        this.operadores.splice(j, 1);
+                    }
+                }
+                console.log("Operador removido");
+                return true;
+            }
+        }
+        console.log("Operador não encontrado!");
+        return false; 
+    }
+
+
+    public listarClientes(): Array<Cliente>{
+        console.log(this.clientes);
+        return this.clientes;
+
+
+
+    }
+
+   public listarOperadores() : Array<OperadorDeSistema>{
+       console.log(this.operadores);
+        return this.operadores;
+    }
+
+    public procurarCliente(email : string) : Cliente |null{
+        for(let usuario of this.mapadeusuario.values()){
+            if(usuario instanceof Cliente && usuario.getEmail()== email){
+                return usuario;
+            }
+        }
+        return null;
+    }
+
+    public adicionarOperador (op:OperadorDeSistema) : void {
+        this.operadores.push(op);
+    }
+    
+    public usuarioLogin(email : string, senha : string) : Usuario | null{
+        for(let usuario of this.usuarios){
+            if(usuario.getEmail() == email && usuario.getSenha() ==  senha){
+                return usuario;
+            }
+        } return null;
+    }
 }
