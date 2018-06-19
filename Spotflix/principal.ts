2114e0247bb7e46007c6320c45e83e384e5569bd
Felipe Venceslau <31327811+venceslaufelipe@users.noreply.github.com>
2018-06-19 00:28:47 -0300

    Add files via upload

--- Spotflix/principal.ts
+++ Spotflix/principal.ts
@@ -14,6 +14,8 @@ import {RepositorioDeProduto} from './usuario/RepositorioDeProduto'
 import {Video} from './video/video'
 import {Filme} from './video/filme'
 import {Serie} from './video/serie'
+import {CartaoDeCredito} from './usuario/cartaoDeCredito';
+import {Audio} from './audio/audio';
 
 
 let userLogado : Usuario | undefined = undefined;
@@ -23,111 +25,258 @@ let repositorioDeUsuario : RepositorioDeUsuario = new RepositorioDeUsuario();
 
 let gerente : Gerente = new Gerente("0101",50000,"Alessandro","ale@mc", "ale123");
 repositorioDeUsuario.usuarios.push(gerente);
-// repositorioDeUsuario.procurarUsuarioLogado("l@gas");
-// console.log(repositorioDeUsuario.procurarUsuarioLogado("l@gas"));
-
-let c : Usuario = new Cliente("rua afonso pena", 10 07 1999, ["01", "bolacha", "comida", 2.00], );
 
 while(true){/*bem vindo*/
-	let op : string = readline.question("*** Bem Vindo ***\n 1- Logar\n 2- Cadastrar-se como cliente\n 3- Sair\n Digite uma opção: ");
+	let op : string = readline.question("*** Bem Vindo ***\n 1- Logar\n 2- Cadastrar-se como cliente\n 3- Sair\n Digite uma opcao: ");
+	console.log("");
 	if(op=="3") break;
 	if(op=="2"){
-		let nome = readline.question("Nome: ");
-		Cliente.setNome(nome);
-		let email = readline.question("Email: ");
-		if(email != Cliente.getNome()){
-			console.log("Email inválido ou já cadastrado!");
-		}else{
-			Cliente.setEmail(email);	
-		}
-		let senha =  readline.question("Senha: ");
-		Cliente.setSenha(senha);
-		let end = readline.question("Endereco: ");
-		Cliente.setEndereco(end);
-		let dat = readline.question("Data de nascimento: ");
-		Cliente.setDataDeNascimento(dat);
+		let nome = readline.question("Digite seu nome: ");
+        let email = readline.question("Digite seu email: ");
+        let senha = readline.question("Digite sua senha: ");
+        let matricula = readline.question("Digite sua matricula: ");
+        let endereco = readline.question("Digite seu endereco: ");
+        let datadeNascimento  : Data = readline.question("Digite sua data de nascimento: ");
+        let listadeProdutos :  Array<Produto> = [];
+        let numeroCartao = readline.question("Digite o numero do cartao de credito: ");
+        let validadeCartao : Data = readline.question("Digite a validade do seu cartao: ");
+        let codigoCCV = readline.question("Digite o codigo o codigo de seguranca(CCV): ");
+        let limiteCartao = readline.question("Limite do seu cartão: ");
+        let cartaoDeCredito : CartaoDeCredito = new CartaoDeCredito(numeroCartao, validadeCartao, codigoCCV, limiteCartao);
+        let clienteUser : Cliente = new Cliente(endereco, datadeNascimento, listadeProdutos, cartaoDeCredito, nome, email, senha);
+        repositorioDeUsuario.clientes.push(clienteUser);
+        repositorioDeUsuario.usuarios.push(clienteUser);
 	}
+
 	if(op=="1"){
 		console.log("****Faça seu Login****");
-		let email1 = readline.quetions("Digite seu login: ");
-		if(email1 != RepositorioDeUsuario.setEmail){
-			console.log("Email inválido")
+		let email = readline.question("Digite seu email: ");
+        let senha = readline.question("Digite sua senha: ");
+
+        let usuario = repositorioDeUsuario.usuarioLogin(email, senha);
+        if(usuario instanceof Gerente){
+            let flag:number = 0;
+
+            while(flag == 0){
+                let comandoGerente : Array<string>;
+                console.log(" ");
+
+                let operacoes : string = "1 - Cadastrar Operador \n 2 - Remover Operador \n 3 - Listar Produtos \n 4 - Listar Clientes \n 5 - Listar Operadores \n 6 - Procurar Produto \n 7 - Procurar Cliente \n 8 - Procurar Operador \n 9 - Sair \n";
+
+                console.log(`Olá gerente ${usuario.getNome()}`);
+                console.log("O que deseja fazer hoje? \n")
+                console.log(operacoes);
+                comandoGerente = readline.question("Digite uma opcao: ").split(" ");
+
+                if(comandoGerente[0] == "1"){
+                    let nomeOperador = readline.question('Digite o nome do operador: ');
+                    let operadorEmail = readline.question("Digite o email do operador: ");
+                    let operadorSenha = readline.question("Digite a senha do operador: ");
+                    let operadorMatricula = readline.question("Digite a matricula do operador: ");
+                    let operadorSalario = readline.question("Digite o salario do operador: ");
+                    
+    
+                    let operador : OperadorDeSistema = new OperadorDeSistema(operadorMatricula, operadorSalario, nomeOperador, operadorEmail, operadorSenha);
+                    repositorioDeUsuario.usuarios.push(operador);
+                    repositorioDeUsuario.operadores.push(operador);
+                }
+                else if(comandoGerente[0] == "2"){
+                    console.log("Remover Operador");
+                    let removeOperador = readline.question("Digite o email do operador: ");
+                    repositorioDeUsuario.removerOperador(removeOperador);
+                }
+
+                else if(comandoGerente[0] == "3"){
+                    console.log("Lista de Produtos: ");
+                    console.log(repositorioDeProdutos.listarProdutos());
+
+                }
+                else if(comandoGerente[0] == "4"){
+                    console.log("Lista de Clientes: \n");
+                    console.log(repositorioDeUsuario.listarClientes());
+                    
+                }
+
+                else if(comandoGerente[0] == "5"){
+                    console.log("Lista de Operadores");
+                    console.log(repositorioDeUsuario.listarOperadores());
+                }
+
+                else if(comandoGerente[0] == "6"){
+                    let procurarProduto = readline.question("Digite o código do produto: ");
+                    if(procurarProduto == repositorioDeProdutos.procurarProduto(procurarProduto)){
+                        console.log("Produto: "+ repositorioDeProdutos.procurarProduto(procurarProduto));
+                    
+                    }
+                }
+                else if(comandoGerente[0] == "7"){
+                    let procuraCliente = readline.question("Digite o cemail do cliente: ");
+                    if(procuraCliente == repositorioDeUsuario.procurarCliente(procuraCliente)){
+                        console.log("Cliente: " + repositorioDeUsuario.procurarCliente(email));
+
+                    }
+                    
+                }
+
+                else if(comandoGerente[0] == "8"){
+                    let procurarOperador = readline.question("Digite o email do operador: ");
+                    if(procurarOperador == repositorioDeUsuario.procurarCliente(procurarOperador)){
+                        console.log("Operador de Sistema: " + repositorioDeUsuario.procurarOperador(email));
+
+                    }
+                }
+
+                else if(comandoGerente[0] == "9"){
+                    flag = 1;
+                }else{ 
+                	flag = 1;
+            	}
+            }
+        }
+
+        else if(usuario instanceof OperadorDeSistema){
+            let flag: number = 0;
+            while(flag == 0){
+                let comandoOperador : Array<string>;
+                console.log(" ");
+    
+                let operacoes : string ="1 - Cadastrar Produto \n 2 - Remover Produto \n 3 - Listar Produtos \n 4 - Listar Clientes \n 5 - Procurar Produto \n 6 - Procurar Cliente \n 7 - Sair";
+    
+                console.log(`Ola Operador ${usuario.getNome()}`);
+                console.log("O que deseja fazer hoje? \n")
+                console.log(operacoes);
+
+                comandoOperador = readline.question("Digite uma opção: ").split(" ");
+                if(comandoOperador[0] == "1"){
+
+                    let listaProdutos : string =  "Voce poderá adicionar esses produtos: \n"+
+                                                "1 - Vídeo \n" +
+                                                "2 - Audio \n" +
+                                                "3 - Pacotes \n";
+                
+                    console.log(listaProdutos);
+
+                    let prodtudoAdiconar = readline.question("Digite a opcao: ").split(" ");
+
+                    if(prodtudoAdiconar[0] == "1"){
+                        let comandoVideo = Array<string>();
+                        let listaVideos : string = "Escolha o tipo de vídeo que deseja adicionar: \n 1 - Filmes \n 2 - Series";
+                        console.log(listaVideos);
+                        
+                        comandoVideo = readline.question("Digite a opção desejada: ").split(" "); 
+
+
+                        if (comandoVideo[0]== "1"){
+                                
+                            let codigoFilme = readline.question("Digite o codigo do filme: ");
+                            let nomeFilme = readline.question("Digite o nome do filme: ");
+                            let generoFilme = readline.question("Digite o genero do filme: ");
+                            let precoFilme = readline.question("Digite o preco do filme: ");;
+                            let anoLancamentoFilme = readline.question("Ano de lancamento do filme: ");
+                            let faixaEtariaFilme = readline.question("Digite a faixa etaria do filme: ");
+                            let duracaoFilme = readline.question("Qual a duracao do filme: ");
+
+                            let vid : Video = new Filme(codigoFilme, nomeFilme, generoFilme, precoFilme, anoLancamentoFilme, faixaEtariaFilme,duracaoFilme);
+                            repositorioDeProdutos.adicionarFilme(codigoFilme, nomeFilme, generoFilme, precoFilme, anoLancamentoFilme, faixaEtariaFilme,duracaoFilme);
+                            console.log("Filme adicionado!");
+                        }
+
+                        if(comandoVideo[0] == "2"){
+                            
+                            let codigoSerie = readline.question("Digite o código da Serie: ");
+                            let nomeSerie = readline.question("Digite o nome da Serie: ");
+                            let generoSerie = readline.question("Digite o genero da Serie: ");
+                            let precoSerie = readline.question("Digite o preco da Serie: ");;
+                            let anoLancamentoSerie = readline.question("Ano de lancamento da Serie: ");
+                            let faixaEtariaSerie = readline.question("Digite a faixa etaria da Serie: ");
+                            let numeroDeEpisodios = readline.question("Digite a quantidade de episodios da serie: ");
+                            let duracaoMediaDeEpisodios = readline.question("Qual a duracao da Serie: ");
+
+                            let serie : Video = new Serie(codigoSerie,nomeSerie,generoSerie,precoSerie,anoLancamentoSerie,faixaEtariaSerie,numeroDeEpisodios,duracaoMediaDeEpisodios);
+                            repositorioDeProdutos.adicionarSerie(codigoSerie,nomeSerie,generoSerie,precoSerie,anoLancamentoSerie,faixaEtariaSerie,numeroDeEpisodios,duracaoMediaDeEpisodios);
+                        	console.log("Serie adicionada!");
+                        }
+
+                        /*if(comandoVideo[0] == "3"){
+                            
+                            let listaProd = readline.question("Digite o nome dos produtos: ").split(",");
+                            let validadeSerie = readline.question("Digite a validade(dd/mm/aaaa): ").split("/");
+
+                            let pacote : Produto = new Pacote();
+                            repositorioDeProdutos.adicionarSerie(codigoSerie,nomeSerie,generoSerie,precoSerie,anoLancamentoSerie,faixaEtariaSerie,numeroDeEpisodios,duracaoMediaDeEpisodios);
+                        	console.log("Serie adicionada!");
+                        }*/
+  
+                    }
+
+                    if(prodtudoAdiconar[0] == "2"){
+                    	console.log("Produto: \n");
+            			console.log(repositorioDeProdutos.listarProdutos());
+            			let codigo = readline.question("Escolha um produto: ");
+            			repositorioDeProdutos.removerProduto(codigo);
+            			console.log(repositorioDeProdutos.listarProdutos()+"\n Produto excluído!");
+                        
+                    }
+                }
+            }
+        }
+
+        else if(usuario instanceof Cliente){
+        	let flag = 0;
+        	while(flag == 0){
+            	let comandoCliente : Array<string>;
+            	console.log(" ");
+            	let operacoes : string ="1 -Listar produtos da loja \n 2 - Listar meus produtos \n 3 - Comprar produto \n 4 - Play \n 5 - Sair \n";
+            	console.log(`Ola cliente ${usuario.getNome()}`);
+            	console.log("O que você  deseja fazer agora? \n");
+            	console.log(operacoes);
+            	comandoCliente = readline.question("Digite uma opcao: ").split(" ");
+
+            	if(comandoCliente[0] == "1"){
+            		console.log(repositorioDeProdutos);
+            	}
+            	if(comandoCliente[0] == "2"){
+            		console.log(repositorioDeProdutos);
+            	}
+            	if(comandoCliente[0] == "3"){
+            		console.log("Produto: \n");
+            		console.log(repositorioDeProdutos.listarProdutos());
+            		let codigo = readline.question("Escolha um produto: ");
+            		repositorioDeProdutos.removerProduto(codigo);
+            		console.log(repositorioDeProdutos.listarProdutos()+"\n Produto comprado!");
+            	}
+            	if(comandoCliente[0] == "4"){
+					let listaProdutos : string =  "Você poderá dar play nesses produtos: \n"+
+            	                                    "1 - Vídeo \n" +
+            	                                    "2 - Audio \n";
+                
+            	    console.log(listaProdutos);
+            	    let comandoPlay = readline.question("Digite uma opção: ").split(" ");
+
+            	    /*if(comandoPlay[0] == "1"){
+            	    	console.log("Filmes: "+ repositorioDeProdutos.mapaFilme());
+            	    	console.log("Séries: " + repositorioDeProdutos.mapaSerie());
+           		     	let op = readline.question("Escolhe um vídeo: ");
+            	    	if(op of repositorioDeProdutos.procurarProduto(op)){
+            	    		console.log(op.executando());
+            	    	}
+            	    }
+            	    if(comandoPlay[0] == "2"){
+            	    	console.log("Musica: "+ repositorioDeProdutos.mapaMusica());
+            	    	console.log("Podcast: " + repositorioDeProdutos.mapaPodcast());
+           		     	let op = readline.question("Escolhe um audio: ");
+            	    	if(op of repositorioDeProdutos.procurarProduto(op)){
+            	    		console.log(op.executando());
+            	    	}
+            	    }*/
+            	}
+            
+            	if(comandoCliente[0] == "5"){
+            		flag = 1;
+            	}
+			}
 		}else{
-			if(Usuario.setEmail(email1) instanceof Gerente){
-				while(true){
-					let opc = readline.question("****Olá Gerente "+ Gerente.getNome() +"*****\n 1 – Cadastrar Operador\n 2 – Remover Operador\n 3 – Listar Produtos\n 4 – Listar Clientes\n 5 – Listar Operadores\n 6 – Procurar Produto\n 7 – Procurar Cliente\n 8 – Procurar Operador\n 9 – Sair\n Digite a opção:")
-					if(opc == "1"){
-
-					}
-					if(opc == "2"){
-
-					}
-					if(opc == "3"){
-
-					}
-					if(opc == "4"){
-
-					}
-					if(opc == "5"){
-
-					}
-					if(opc == "6"){
-
-					}
-					if(opc == "7"){
-
-					}
-					if(opc == "8"){
-
-					}
-					if(opc == "9") break;
-				}
-			}
-
-			if(Usuario.setEmail(email1) instanceof OperadorDeSistema){
-				while(true){
-					let opc = readline.question("****Olá Gerente "+ OperadorDeSistema.getNome() +"*****\n 1 – Cadastrar Produto\n 2 – Remover Produto\n 3 – Listar Produtos\n 4 – Listar Clientes\n 5 – Procurar Produto\n 6 – Procurar Cliente\n 7 – Sair\n Digite a opção:")
-					if(opc == "1"){
-
-					}
-					if(opc == "2"){
-
-					}
-					if(opc == "3"){
-
-					}
-					if(opc == "4"){
-
-					}
-					if(opc == "5"){
-
-					}
-					if(opc == "6"){
-
-					}
-					if(opc == "7") break;
-				}
-			}
-			if(Usuario.setEmail(email1) instanceof Cliente){
-				while(true){
-					let opc = readline.question("****Olá Gerente "+ Cliente.getNome() +"*****\n 1 – Listar Produtos da Loja\n 2 – Listar Meus Produtos\n 3 – Comprar Produto\n 4 – Play\n 5 – Sair\n Digite a opção:")
-					if(opc == "1"){
-
-					}
-					if(opc == "2"){
-
-					}
-					if(opc == "3"){
-
-					}
-					if(opc == "4"){
-
-					}
-					if(opc == "5") break;
-				}
-			}
+			console.log("Usuário não existe!");
 		}
-		let senha1 = readline.quetions("Digite sua senha: ");
-		Usuario.setSenha(senha1);
 	}
 }
